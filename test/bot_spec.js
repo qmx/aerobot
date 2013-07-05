a15ed2ba057e5db28159cef6e1e2fe66664fd362
Douglas Campos <qmx@qmx.me>
2013-07-05 13:19:52 -0300

    remove pointless test

--- test/bot_spec.js
+++ test/bot_spec.js
@@ -2,10 +2,6 @@ var should = require('should');
 var Bot = require('../lib/bot').Bot;
 
 describe('The Bot', function () {
-    it('holds basic configuration', function () {
-        var bot = new Bot('lulz');
-        bot.nick.should.equal('lulz');
-    });
     it('knows when a message is addressed to himself', function() {
         var bot = new Bot('lolz');
         bot.addressedToMe('lolz: welcome to the jungle').should.be.ok;
