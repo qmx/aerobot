5a406b6f840e93611e85430052b3804170647f89
Douglas Campos <qmx@qmx.me>
2013-07-17 15:48:54 -0300

    externalize config to ENV

--- bot.js
+++ bot.js
@@ -11,43 +11,50 @@ if (process.env.REDIS_URL) {
     var client = redis.createClient();
 }
 
-var options = {
-    host:'irc.freenode.net',
-    nick:'aerobot2',
-    channels:['#aerobot-test']
-};
+if (process.env.AEROBOT_CONFIG) {
+    var config = JSON.parse(process.env.AEROBOT_CONFIG);
+} else {
+    var config = {
+        irc:{
+            host:'irc.freenode.net',
+            nick:'aerobot33',
+        },
+        channels: ['#aerobot-test']
+    };
+}
 
-var ircConnection  = new irc.Client(options.host, options.nick, {
-    channels: options.channels
+var ircConnection  = new irc.Client(config.irc.host, config.irc.nick, {
+    channels: config.irc.channels
 });
 
-var bot = new Bot(options.nick);
+var bot = new Bot(config.irc.nick);
+
 ircConnection.addListener('message', function (from, to, message) {
     if(bot.isStatusMessage(message)) {
-        var key = "aerobot:status:" + options.host + ":" + util.normalizeChannelName(to) + ":" + from;
+        var key = "aerobot:status:" + config.irc.host + ":" + util.normalizeChannelName(to) + ":" + from;
         client.hset(key, new Date().toISOString(), bot.normalizeMessage(message), function (err, reply){
             ircConnection.say(to, from + ': recorded your slacking at <dashboard link coming soon>');
         });
     } else if(bot.isFactoidStoreRequest(message)) {
-        var key = "aerobot:factoid:" + options.host + ":" + util.normalizeChannelName(to);
+        var key = "aerobot:factoid:" + config.irc.host + ":" + util.normalizeChannelName(to);
         var request = bot.parseFactoidStoreRequest(message);
         client.hset(key, request.key, request.value, function (err, reply) {
             ircConnection.say(to, from + ': kk');
         });
     } else if(bot.isFactoidRemovalRequest(message)) {
-        var key = "aerobot:factoid:" + options.host + ":" + util.normalizeChannelName(to);
+        var key = "aerobot:factoid:" + config.irc.host + ":" + util.normalizeChannelName(to);
         var request = bot.parseFactoidRemovalRequest(message);
         client.hdel(key, request, function (err, reply) {
             ircConnection.say(to, from + ': never heard of it!');
         });
     } else if(bot.isFactoidRetrievalRequest(message)) {
-        var key = "aerobot:factoid:" + options.host + ":" + util.normalizeChannelName(to);
+        var key = "aerobot:factoid:" + config.irc.host + ":" + util.normalizeChannelName(to);
         var request = bot.parseFactoidRetrievalRequest(message);
         client.hget(key, request, function (err, reply) {
             ircConnection.say(to, from + ': ' + reply);
         });
     } else if(bot.isKarmaRequest(message)) {
-        var key = "aerobot:karma:" + options.host + ":" + util.normalizeChannelName(to);
+        var key = "aerobot:karma:" + config.irc.host + ":" + util.normalizeChannelName(to);
         var request = bot.parseKarmaRequest(message);
         client.hincrby(key, request.user, request.direction, function (err, reply) {
             var actionText = request.direction === 1 ? 'gained' : 'lost';
