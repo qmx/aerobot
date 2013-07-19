d16038243de4122796e8ef54a07ace6e691e6c6b
Douglas Campos <qmx@qmx.me>
2013-07-19 16:25:24 -0300

    consistency is king

--- test/util_spec.js
+++ test/util_spec.js
@@ -28,9 +28,9 @@ describe('Util module', function() {
             {user:'summersbot',score:0},
             {user:'summersbot1',score:-15}
         ];
-        
+
         var scoresString = "summersp         1\nsummersbot       0\nsummersbot1    -15\n";
-        
+
         var lines = util.prettyPrintKarmaScores(scores);
         lines.should.eql(scoresString);
     });
@@ -75,38 +75,41 @@ describe('Util module', function() {
         };
         util.parseStatuses(redisOutput).should.eql({
             "aerobot:status": {
-                "irc.freenode.net": {
-                    channels: [
-                        {
-                            channel: "#aerobot-test",
-                            users: [
-                                {
-                                    user: "qmx",
-                                    statuses: [
-                                        {
-                                            timestamp: "2013-07-10T17:15:42.198Z",
-                                            status: "super boring day for a #status"
-                                        },
-                                        {
-                                            timestamp: "2013-07-10T17:16:25.967Z",
-                                            status: "medium rare with aged cheddar and mushrooms #status"
-                                        }
+                "networks": [
+                    {
+                        network: "irc.freenode.net",
+                        channels: [
+                            {
+                                channel: "#aerobot-test",
+                                users: [
+                                    {
+                                        user: "qmx",
+                                        statuses: [
+                                            {
+                                                timestamp: "2013-07-10T17:15:42.198Z",
+                                                status: "super boring day for a #status"
+                                            },
+                                            {
+                                                timestamp: "2013-07-10T17:16:25.967Z",
+                                                status: "medium rare with aged cheddar and mushrooms #status"
+                                            }
 
-                                    ]
-                                },
-                                {
-                                    user: "whatever",
-                                    statuses: [
-                                        {
-                                            timestamp: "2013-07-11T17:01:33.123Z",
-                                            status: "who doesn't care about cheese? #status"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
+                                        ]
+                                    },
+                                    {
+                                        user: "whatever",
+                                        statuses: [
+                                            {
+                                                timestamp: "2013-07-11T17:01:33.123Z",
+                                                status: "who doesn't care about cheese? #status"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
         });
     });
