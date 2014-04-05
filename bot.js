57e9b9737234bcc8936a8d6c7fca792be1eabfba
Douglas Campos <qmx@qmx.me>
2014-04-05 14:00:21 -0300

    cleaning up old redis stuff
    references #12

--- bot.js
+++ bot.js
@@ -1,18 +1,10 @@
 var irc = require('irc');
 var Bot = require('./lib/bot').Bot;
 var util = require('./lib/util');
-var redis = require('redis');
 var _ = require('lodash');
 var requireDir = require('require-dir');
 var handlers = requireDir('./lib/handlers');
 var db = require('./models');
-if (process.env.REDIS_URL) {
-    var redisURL = require('url').parse(process.env.REDIS_URL);
-    var client = redis.createClient(redisURL.port, redisURL.hostname);
-    client.auth(redisURL.auth.split(":")[1]);
-} else {
-    var client = redis.createClient();
-}
 
 if (process.env.AEROBOT_CONFIG) {
     var config = JSON.parse(process.env.AEROBOT_CONFIG);
@@ -34,6 +26,6 @@ var bot = new Bot(config.irc.nick);
 
 ircConnection.addListener('message', function (from, to, message) {
     for (var handler in handlers) {
-        handlers[handler](config, ircConnection, bot, db, client, from, to, message);
+        handlers[handler](config, ircConnection, bot, db, from, to, message);
     }
 });
